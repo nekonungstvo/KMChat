c41d7233779e326e4543843d696a3313103573f0
volpe262 <volpe_262@mail.ru>
2017-09-27 23:43:46 +0200

    Add getter for skills

--- src/main/java/KMChat/KMChat.java
+++ src/main/java/KMChat/KMChat.java
@@ -11,13 +11,14 @@ import java.util.logging.Logger;
 import java.util.Date;
 import java.util.regex.Matcher;  
 import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.nio.file.*;
+import java.nio.charset.*;
 import java.io.*;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Server;
-import org.bukkit.World;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.FileConfigurationOptions;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -50,13 +51,14 @@ extends JavaPlugin
 implements Listener {
     private static String TOKEN;
     private static String CHID;
+    private static String DATA_PATH;
     private static IDiscordClient client;
     private static IChannel ingameChannel;
     private Logger log = Logger.getLogger("Minecraft");
     private String path = "logs/";
     private Map<Integer, String> nMap = new Hashtable<Integer, String>();
     private Range[] allRanges = new Range[6];
-
+    private String[] skillset;
     public void onEnable() {
 
         this.getConfig().options().copyDefaults(true);
@@ -65,8 +67,9 @@ implements Listener {
         path = this.getConfig().getString("logsdir");
         TOKEN = this.getConfig().getString("bottoken");
         CHID = this.getConfig().getString("channelid");
-        
-        System.out.println("Logging bot in...");
+        DATA_PATH = this.getConfig().getString("datastorage");
+         
+	System.out.println("Logging bot in...");
         client = new ClientBuilder().withToken(TOKEN).build();
         client.getDispatcher().registerListener(this);
         client.login();
@@ -87,8 +90,10 @@ implements Listener {
 	allRanges[5] = weakshout;     //!   order is important
 
         this.getServer().getPluginManager().registerEvents((Listener)this, (Plugin)this);
-        this.nMap.put(-3, "так ужасно, что хуже уже некуда");
+	//this.nMap.put(-3, "так ужасно, что хуже уже некуда");
+	this.nMap.put(-3, "абсолютно ублюдски");
         this.nMap.put(-2, "ужасно---");
+        this.nMap.put(-1, "ужасно--");
         this.nMap.put(-1, "ужасно--");
         this.nMap.put(0, "ужасно-");
         this.nMap.put(1, "ужасно");
@@ -101,8 +106,14 @@ implements Listener {
         this.nMap.put(8, "легендарно");
         this.nMap.put(9, "легендарно+");
         this.nMap.put(10, "легендарно++");
-        this.nMap.put(11, "легендарно+++");
-        this.nMap.put(12, "ТАК ЛЕГЕНДАРНО, ЧТО ПОПАДЁТ ВО ВСЕ КНИГИ РЕКОРДОВ");
+        this.nMap.put(11, "легендарно+++");	
+        //this.nMap.put(12, "ТАК ЛЕГЕНДАРНО, ЧТО ПОПАДЁТ ВО ВСЕ КНИГИ РЕКОРДОВ");
+        this.nMap.put(12, "божественно");
+
+	skillset = new String [] {"реакция", "владение оружием", "кулачный бой", "борьба", "парирование", "уклонение", "блокирование",
+		                    "бег", "плавание", "акробатика", "физическая сила", "выносливость", "устойчивость к болезням", "внимательность",
+				    "скрытность", "выживание", "диагностика", "первая помощь", "зашивание ран", "хирургия"};
+
         this.log.info(String.format("%s is enabled!", this.getDescription().getFullName()));
     }
 
@@ -141,50 +152,6 @@ implements Listener {
 	RequestBuffer.request(() -> ingameChannel.sendMessage(message.replaceAll(name, "__"+name+"__")));
     }
 
-    @EventHandler
-    public void onChatTab(PlayerChatTabCompleteEvent playerChatTabCompleteEvent) {
-    	String mes = playerChatTabCompleteEvent.getChatMessage();
-    	if (mes.startsWith("% ") ||
-	    mes.startsWith("-% ") ||
-            mes.startsWith("=% ") ||
-	    mes.startsWith("!% ") ||
-	    mes.startsWith("==% ") ||
-	    mes.startsWith("!!% ") ||
-	    mes.startsWith("===% ") ||
-	    mes.startsWith("!!!% ") ) {
-
-	    Collection<String> collection = playerChatTabCompleteEvent.getTabCompletions();
-	    collection.clear();
-
-	    if (playerChatTabCompleteEvent.getLastToken().startsWith("у")) {
-		collection.add("ужасно");
-	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("пр")) {
-		collection.add("превосходно");
-	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("пл")) {
-	        collection.add("плохо");
-	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("по")) {
-		collection.add("посредственно");
-	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("п")) {
-		collection.add("плохо");
-		collection.add("посредственно");
-		collection.add("превосходно");
-	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("н")) {
-		collection.add("нормально");
-	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("х")) {
-		collection.add("хорошо");
-	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("о")) {
-		collection.add("отлично");
-	    } else {
-		collection.add("ужасно");
-		collection.add("плохо");
-		collection.add("посредственно");
-		collection.add("нормально");
-		collection.add("хорошо");
-		collection.add("отлично");
-		collection.add("превосходно");
-	    }	
-	}
-    }	
 
 
     @EventHandler
@@ -192,10 +159,12 @@ implements Listener {
 	//Long beg = System.nanoTime();
 	Player player = asyncPlayerChatEvent.getPlayer();
 	int rangePosition = 6; // if no range specified, lands on the last element of array with descriptions
-	boolean local = true; 
-	boolean forgm = false; //to GMchat
+
+	boolean local = true;
+	boolean forgm = false; //to gm chat
 	boolean norec = false; //no recipients at all
-	boolean sendraw = false;
+	boolean sendraw = false; //either to send a raw message to discord
+
 	String mes = asyncPlayerChatEvent.getMessage();
 	String raw = mes;
 	String name = player.getName();
@@ -265,7 +234,7 @@ implements Listener {
 	    } else {
 		mes = mes.substring(1);
 	    }
-	    String dice = dF(mes);
+	    String dice = dF(mes, player.getName());
 	    if (dice == null) {
 		player.sendMessage("§4Для броска дайса пропишите: \"% значение\"§f");	
 		norec = true;
@@ -281,7 +250,7 @@ implements Listener {
 	    } else {
 		mes = mes.substring(2);
 	    }
-	    String dice = dF(mes);
+	    String dice = dF(mes, player.getName());
 	    if (dice == null) {
 		player.sendMessage("§4Для броска дайса пропишите: \"% значение\"§f");	
 		norec = true;
@@ -340,7 +309,7 @@ implements Listener {
 	}
 
 	if (mes.startsWith("((") && mes.endsWith("))")) {
-	    String str = "";
+            String str = "";
 	    if (setRange != null) {
 		str = setRange.getDescription().replaceAll("[),(, ]","") + " в ";
 		if (rangePosition == 0) {
@@ -355,6 +324,7 @@ implements Listener {
 
 	result = result.replaceAll("%", "%%");
 	result = result.replaceAll("&([a-z0-9])", "§$1");
+	result = result.replaceAll("\\]\\s\\.", "].");
 	asyncPlayerChatEvent.setFormat(result);
 	asyncPlayerChatEvent.setMessage(mes);
 	kmlog("whole", result);
@@ -395,7 +365,9 @@ implements Listener {
     
     
     //---}}} Various helpers
+    
     protected List<Player> getLocalRecipients(Player player, String mes, double d) {
+    
     
     
 	Location location = player.getLocation();
@@ -434,18 +406,170 @@ implements Listener {
 	}
     }
 
-    protected String dF(String mes) {
-	String result = "(";
-	int n = -1;
+    protected String getSkill(String desc, String nick) {
+	//we need to have a file "Playernick.skills" with skills listed as following: "skill:level"
+	Path path = Paths.get(DATA_PATH, nick + ".skills");
+	Charset charset = Charset.forName("UTF-8");
+	Pattern skillpat = Pattern.compile("(.*):");
+	Pattern levelpat = Pattern.compile(":(.*)");
+
+	String skill = null;
+	String level = null;
+
+        try {
+	    List<String> lines = Files.readAllLines(path, charset);
+
+	    for (String line : lines) {
+		Matcher skillmat = skillpat.matcher(line);
+		while (skillmat.find()) {
+		    skill = skillmat.group().replaceAll(":",""); //get the name of the skill
+		}
+		if (desc.toLowerCase().startsWith(skill)) {
+		    Matcher levelmat = levelpat.matcher(line);
+		    while (levelmat.find()) {
+		        level = levelmat.group(); //get the level of the skill
+		    }
+		    break;
+		} else {
+			skill = null;
+		}
+	    }
+	    if (skill == null) {
+	        for (String sk : skillset) {
+	            if (desc.toLowerCase().startsWith(sk)) {
+		        skill = sk;
+	    //System.out.println("skill is " + skill + ", level is " + level);
+                        break;
+		    } else {
+                        level = null;
+                    }
+	        }
+	    }
+
+	} catch (IOException e) {
+	    System.out.println(e);
+	    level = null;
+	}
+	    if (level == null) {
+		level = ":ПЛОХО";
+            }
+//                        System.out.println("skill is " + skill + ", level is " + level);
+            if (skill == null)
+                return null;
+	    String result = desc.replaceFirst(skill, level + " ["+skill+"] (");
+	    //let's make the output pretty!
+	    //System.out.println(result);
+	    //System.out.println(desc);
+
+	    result = result.replaceFirst("\\(\\s", "\\(");
+	    result = result.substring(1, result.length()) + ")";
+	    if (result.contains("()"))
+		result = result.replaceFirst("\\(\\)", "");
+	    return result;
+
+    }
+
+    public int getSkillValue(String levelName) {
 	for (Map.Entry<Integer, String> entry : nMap.entrySet()) {
-	    if (mes.startsWith(entry.getValue())) {
+            if (levelName.equals(entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return 666;
+    }
+
+    public boolean containsSkillValue(String message) {
+        for (Map.Entry<Integer, String> entry : nMap.entrySet()) {
+            if (message.startsWith(entry.getValue())) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public String dF(String mes, String nick) {
+	boolean weusedskill = false;
+	String result = "(";
+	int n = 666; //skill level represented as number
+	float mod = 0; //optinal modificator to skill level
+
+        if (!containsSkillValue(mes)) {
+		//int wordsNum = 1;
+		//String[] split = mes.split(" ", wordsNum + 2); //extra spot for optional modificator
+                String skillmes = getSkill(mes, nick);
+                if (skillmes != null) {
+		    mes = skillmes;
+		
+	        Pattern ourSkillPat = Pattern.compile("\\(([-,+][0-9]).*");
+                Matcher ourSkillMat = ourSkillPat.matcher(skillmes);
+                String strmod = null;
+                if (ourSkillMat.find()) {
+                    strmod = ourSkillMat.group(1);
+                }
+
+		if (strmod != null) {
+		    //String strmod = split[wordsNum];
+		    if (strmod.matches("[-,+][0-9]")) {
+			mes = mes.replace( "] (" + strmod, " " + strmod + "] (");
+			if (mes.contains("()"))
+				mes = mes.replaceFirst("\\(\\)", "");
+			mod = Float.parseFloat(strmod);
+		    }
+		}
+		weusedskill = true;
+                }
+	    }
+	
+	String level = "";
+	String comment = "";
+	try {
+	    level = mes.split(" ", 2)[0];
+	    if (!weusedskill) { //if we used skill, comment is already inside (braces)
+		comment = mes.split(" ", 2)[1];
+		mes = mes.replace(comment, "("+comment+")");
+	    }
+	} catch (Exception e) {
+	    System.out.println("dF() caugth exception: " + e);
+	    level = mes;
+	}
+	/*
+	for (Map.Entry<Integer, String> entry : nMap.entrySet()) {
+	    if (level.equals(entry.getValue())) {
 		n = entry.getKey();
 		break;
 	    }
 	}
-        if (n < 0) {
+	*/
+	n = getSkillValue(level);
+	if (level.equals("ПЛОХО"))
+		n = 2;
+
+	if (n == 666) {
 	    return null;
+        } else if (n < 1) {
+		n = 1;
+	} else if (n > 8) {
+		n = 8;
 	}
+
+	int oldn = n;
+	n = n + Math.round(mod);
+
+	if (n < -2) {
+		n = -2;
+	} else if (n > 11) {
+		n = 11;
+	}
+	if (mod != 0) {
+		try {
+			mes = mes.replaceFirst(level, nMap.get(n));
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
 	int[] dices = {-1, 0, 1};
 	int dice = 0;
 	for (int i = 0; i < 4; ++i) {
@@ -463,9 +587,19 @@ implements Listener {
 		result += "=";
 	    }
 	}
-	result += String.format(") от %s. Результат: %s", mes, nMap.get(n)); 
+	if (n < -3) {
+		n = -3;
+	} else if (n > 12) {
+		n = 12;
+	}
+	result += String.format(") от %s. Результат: %s", mes, nMap.get(n));
+	result = result.replace("( ", "("); 
 	return result; 
     }	
+
+    public String dF(String mes) {
+	return dF(mes, "");
+    }
 
     protected String dnum(String mes) {
 	int[] poss = {4, 6, 8, 10, 12, 14, 20};
@@ -682,4 +816,130 @@ implements Listener {
     }
     //---}}} IngameBot
     
+    //---{{{ Tabs
+        @EventHandler
+    public void onChatTab(PlayerChatTabCompleteEvent playerChatTabCompleteEvent) {
+    	String mes = playerChatTabCompleteEvent.getChatMessage();
+    	if (mes.startsWith("% ") ||
+	    mes.startsWith("-% ") ||
+            mes.startsWith("=% ") ||
+	    mes.startsWith("!% ") ||
+	    mes.startsWith("==% ") ||
+	    mes.startsWith("!!% ") ||
+	    mes.startsWith("===% ") ||
+	    mes.startsWith("!!!% ") ) {
+
+	    Collection<String> collection = playerChatTabCompleteEvent.getTabCompletions();
+	    collection.clear();
+
+	    if (playerChatTabCompleteEvent.getLastToken().startsWith("у")) {
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("уж")) {
+		collection.add("ужасно");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("ук")) {
+                collection.add("уклонение");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("ус")) {
+                collection.add("устойчивость к болезням");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("у")) {
+                collection.add("устойчивость к болезням");
+		collection.add("ужасно");
+                collection.add("уклонение");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("а")) {
+                collection.add("акробатика");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("бe")) {
+                collection.add("бег");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("бл")) {
+                collection.add("блокирование");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("бо")) {
+                collection.add("борьба");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("б")) {
+                collection.add("борьба");
+                collection.add("блокирование");
+                collection.add("бег");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("вын")) {
+                collection.add("выносливость");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("выж")) {
+                collection.add("выживание");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("вы")) {
+                collection.add("выносливость");
+                collection.add("выживание");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("вл")) {
+                collection.add("владение оружием");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("вн")) {
+                collection.add("внимательность");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("в")) {
+                collection.add("владение оружием");
+                collection.add("выносливость");
+                collection.add("внимательность");
+                collection.add("выживание");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("д")) {
+                collection.add("диагностика");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("з")) {
+                collection.add("зашивание ран");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("р")) {
+                collection.add("реакция");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("к")) {
+                collection.add("кулачный бой");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("ф")) {
+                collection.add("физическая сила");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("с")) {
+                collection.add("скрытность");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("х")) {
+                collection.add("хирургия");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("пр")) {
+		collection.add("превосходно");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("пла")) {
+                collection.add("плавание");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("пло")) {
+                collection.add("плохо");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("пл")) {
+                collection.add("плохо");
+                collection.add("плавание");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("по")) {
+		collection.add("посредственно");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("п")) {
+		collection.add("плохо");
+		collection.add("посредственно");
+		collection.add("превосходно");
+                collection.add("парирование");
+                collection.add("плавание");
+                collection.add("первая помощь");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("н")) {
+		collection.add("нормально");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("х")) {
+		collection.add("хорошо");
+	    } else if (playerChatTabCompleteEvent.getLastToken().startsWith("о")) {
+		collection.add("отлично");
+	    } else {
+		collection.add("ужасно");
+		collection.add("плохо");
+		collection.add("посредственно");
+		collection.add("нормально");
+		collection.add("хорошо");
+		collection.add("отлично");
+		collection.add("превосходно");
+
+                collection.add("реакция");
+                collection.add("владение оружием");
+                collection.add("кулачный бой");
+                collection.add("борьба");
+                collection.add("парирование");
+                collection.add("уклонение");
+                collection.add("блокирование");
+                collection.add("бег");
+                collection.add("плавание");
+                collection.add("акробатика");
+                collection.add("физическая сила");
+                collection.add("выносливость");
+                collection.add("устойчивость к болезням");
+                collection.add("внимательность");
+                collection.add("скрытность");
+                collection.add("выживание");
+                collection.add("диагностика");
+                collection.add("первая помощь");
+                collection.add("зашивание ран");
+                collection.add("хирургия");
+	    }	
+	}
+    }	
+//---}}} Tabs
 }
