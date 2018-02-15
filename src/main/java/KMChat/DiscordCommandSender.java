aa2513f6658f2f2ca56f50c63c98ff53f47fd3a6
xunto <hunterdelyx1@gmail.com>
2018-02-15 22:45:48 +0300

    Implement DiscordCommandSender

---
+++ src/main/java/KMChat/DiscordCommandSender.java
@@ -0,0 +1,137 @@
+package KMChat;
+
+import org.bukkit.Server;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.conversations.Conversation;
+import org.bukkit.conversations.ConversationAbandonedEvent;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.plugin.Plugin;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.util.RequestBuffer;
+
+import java.util.Set;
+
+public class DiscordCommandSender implements ConsoleCommandSender {
+    private IChannel ingameChannel;
+
+    public DiscordCommandSender(IChannel ingameChannel) {
+        this.ingameChannel = ingameChannel;
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        RequestBuffer.request(() -> ingameChannel.sendMessage(message));
+
+    }
+
+    @Override
+    public void sendMessage(String[] strings) {
+        RequestBuffer.request(() -> ingameChannel.sendMessage(String.join("\n", strings)));
+    }
+
+    @Override
+    public Server getServer() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return "Discord";
+    }
+
+    @Override
+    public boolean isConversing() {
+        return false;
+    }
+
+    @Override
+    public void acceptConversationInput(String s) {
+
+    }
+
+    @Override
+    public boolean beginConversation(Conversation conversation) {
+        return false;
+    }
+
+    @Override
+    public void abandonConversation(Conversation conversation) {
+
+    }
+
+    @Override
+    public void abandonConversation(Conversation conversation, ConversationAbandonedEvent conversationAbandonedEvent) {
+
+    }
+
+    @Override
+    public void sendRawMessage(String s) {
+
+    }
+
+    @Override
+    public boolean isPermissionSet(String s) {
+        return true;
+    }
+
+    @Override
+    public boolean isPermissionSet(Permission permission) {
+        return true;
+    }
+
+    @Override
+    public boolean hasPermission(String s) {
+        return true;
+    }
+
+    @Override
+    public boolean hasPermission(Permission permission) {
+        return true;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, String s, boolean b) {
+        return null;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin) {
+        return null;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, String s, boolean b, int i) {
+        return null;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, int i) {
+        return null;
+    }
+
+    @Override
+    public void removeAttachment(PermissionAttachment permissionAttachment) {
+
+    }
+
+    @Override
+    public void recalculatePermissions() {
+
+    }
+
+    @Override
+    public Set<PermissionAttachmentInfo> getEffectivePermissions() {
+        return null;
+    }
+
+    @Override
+    public boolean isOp() {
+        return true;
+    }
+
+    @Override
+    public void setOp(boolean b) {
+    }
+}
